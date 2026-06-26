d8b21ec809c8a5115884d90cb164ca6dd123aa3c
EkKrish2809 <ekbotekishor115@gmail.com>
2026-06-26 10:42:34 +0530

    Day 7: added '#include' directive support in glsl shaders

---
+++ src/shaders/mesh.h
@@ -0,0 +1,13 @@
+struct Vertex{
+    float16_t vx, vy, vz, vw;
+    uint8_t nx, ny, nz, nw;
+    float16_t tu, tv;
+};
+
+struct Meshlet{
+    uint vertices[64];
+    uint8_t indices[126*3];   // up to 126 triangles
+    // uint indicesPacked[124*3/4];   // up to 126 triangles
+    uint8_t triangleCount;
+    uint8_t vertexCount;
+};
